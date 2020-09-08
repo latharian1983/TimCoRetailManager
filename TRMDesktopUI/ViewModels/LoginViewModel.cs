f31cefc84ba2960969d8ad989f3028d3654a2da0
Adam Wall <adam.wall@outook.com>
2020-09-08 14:27:21 -0500

    Wired up login form to API
    Connects the login form button to the Authentication API Endpoint (/token). Gets back the Bearer token or exception if failed.

---
+++ TRMDesktopUI/ViewModels/LoginViewModel.cs
@@ -0,0 +1,58 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using TRMDesktopUI.Helpers;
+
+namespace TRMDesktopUI.ViewModels
+{
+    public class LoginViewModel : Screen
+    {
+        private string _userName;
+        private string _password;
+        private IAPIHelper _apiHelper;
+
+        public LoginViewModel(IAPIHelper apiHelper)
+        {
+            _apiHelper = apiHelper;
+        }
+
+        public string UserName
+        {
+            get { return _userName; }
+            set
+            {
+                _userName = value;
+                NotifyOfPropertyChange(() => CanLogIn);
+            }
+        }
+
+        public string Password
+        {
+            get { return _password; }
+            set
+            {
+                _password = value;
+                NotifyOfPropertyChange(() => CanLogIn);
+            }
+        }
+
+        public bool CanLogIn
+        {
+            get
+            {
+                if (UserName?.Length > 0 && Password?.Length > 0)
+                    return true;
+                return false;
+            }
+        }
+
+        public async Task LogIn()
+        {
+            var result = await _apiHelper.Authenticate(UserName, Password);
+        }
+    }
+}
